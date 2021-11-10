04c811ce93eb4c8fd916725c8828298f9ad241af
Niriot <lopezpatyyarely@gmail.com>
2021-11-10 11:12:52 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 import Shido from "../src/components/Shido";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
     <Shido />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
